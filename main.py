7495a74b353d0875061ee24200c457ac4204cec7
Didacusxv <diogofariasar@gmail.com>
2026-03-04 07:35:38 -0300

    Teste de Notas

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-x = "Porra nenhuma"
+Nomes = ["Joao","Maria","Jose"]
+Notas = [7,3,10]
+S = (0)
 
-print = x
-print = "aaaaaaa"
+while S < 3:
+    if Notas[S] >= 7:
+        print(Nomes[S], "passou com", Notas[S])
+    else:
+        print(Nomes[S], "nao passou com", Notas[S])
+    S = S + 1
